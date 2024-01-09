80927ad32f718f86d4293a05f4ab21709f471be9
Shoaib <shoaibs411@gmail.com>
2024-01-10 01:32:11 +0530

    Provisioning DB's - TF AutoPush

--- redis.tf
+++ redis.tf
@@ -2,7 +2,7 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-${var.ENV}-redis"
   engine               = "redis"
-  node_type            = "cache.t3.micro"
+  node_type            = var.REDIS_ENGINE
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = "6.x"
@@ -21,3 +21,5 @@ resource "aws_elasticache_subnet_group" "redis" {
   name       = "roboshop-${var.ENV}-redis-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
+
+
