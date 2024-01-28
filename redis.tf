5b24f612c3975b8167032639d6dd70e8d8af2896
Shoaib <shoaibs411@gmail.com>
2024-01-29 02:13:02 +0530

    TF Infra AutoPush - Attaching Security Group to redis

--- redis.tf
+++ redis.tf
@@ -1,25 +1,26 @@
 # Provisioning Redis Cluster
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "roboshop-${var.ENV}-redis"
-  engine               = "redis"
-  node_type            = var.REDIS_ENGINE
-  num_cache_nodes      = 1
-  parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = var.REDIS_ENGINE_VERSION
-  port                 = var.REDIS_PORT
-  subnet_group_name    = aws_elasticache_subnet_group.redis.name
+  cluster_id            = "roboshop-${var.ENV}-redis"
+  engine                = "redis"
+  node_type             = var.REDIS_ENGINE
+  num_cache_nodes       = 1
+  parameter_group_name  = aws_elasticache_parameter_group.default.name
+  engine_version        = var.REDIS_ENGINE_VERSION
+  port                  = var.REDIS_PORT
+  subnet_group_name     = aws_elasticache_subnet_group.redis.name
+  security_group_ids    = [aws_security_group.allows_redis.id]
 }
 
 # Parameter Group
 resource "aws_elasticache_parameter_group" "default" {
-  name   = "cache-params"
-  family = var.REDIS_ENGINE_FAMILY
+  name                  = "cache-params"
+  family                = var.REDIS_ENGINE_FAMILY
 }
 
 # Creates Subnet Group
 resource "aws_elasticache_subnet_group" "redis" {
-  name       = "roboshop-${var.ENV}-redis-subnet-group"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+  name                  = "roboshop-${var.ENV}-redis-subnet-group"
+  subnet_ids            = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
 
 
