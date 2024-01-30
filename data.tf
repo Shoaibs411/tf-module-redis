1ba270ba13c06dcf0d75b22ffb53262fb95a1cb8
Shoaib <shoaibs411@gmail.com>
2024-01-30 20:27:16 +0530

    TF Infra AutoPush - Updated S3 Bucket details & some minor updates

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "b56-terraform-state--bucket"
+    bucket  = "roboshop-terraform-state--bucket"
     key     = "${var.ENV}/vpc/terraform.tfstate"
     region  = "us-east-1"
   }
--- route53.tf
+++ route53.tf
@@ -1,15 +1,15 @@
 resource "aws_route53_record" "redis_dns_record" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID 
-  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
-  type    = "CNAME"
-  ttl     = 10
-  records = [aws_elasticache_cluster.redis.cache_nodes.0.address]
+  zone_id   = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID 
+  name      = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type      = "CNAME"
+  ttl       = 10
+  records   = [aws_elasticache_cluster.redis.cache_nodes.0.address]
 }
 
 output "redis_op" {
-  value = aws_elasticache_cluster.redis
+  value     = aws_elasticache_cluster.redis
 }
 
 output "REDIS_ENDPOINT" {
-  value = aws_elasticache_cluster.redis.cache_nodes.0.address
+  value     = aws_elasticache_cluster.redis.cache_nodes.0.address
 }
